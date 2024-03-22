c344f47640a4c3d61416f036dd9ff3d9a0e122fb
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-22 13:44:39 +0200

    updating-app.js-to-be-dynamic

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ class App extends Component {
       monsters: [
         {
           name: "Linda",
+          id: "123e12ed",
         },
         {
           name: "Frank",
+          id: "2123wew12",
         },
         {
           name: "Jacky",
+          id: "123e12ddas",
         },
         {
           name: "Andrei",
+          id: "123e12dsasd",
         },
       ],
     };
@@ -27,7 +31,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <div key={monster.id}><h1>{monster.name}</h1></div>;
         })}
       </div>
     );
