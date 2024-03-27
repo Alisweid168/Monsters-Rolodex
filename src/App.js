1b17dd967fe9134ed1fe5eac3deca13f1aaf5e90
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-27 11:36:29 +0200

    updateing-app.js

--- src/App.js
+++ src/App.js
@@ -6,32 +6,33 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id: "123e12ed",
-        },
-        {
-          name: "Frank",
-          id: "2123wew12",
-        },
-        {
-          name: "Jacky",
-          id: "123e12ddas",
-        },
-        {
-          name: "Andrei",
-          id: "123e12dsasd",
-        },
-      ],
+      monsters: [],
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <div key={monster.id}><h1>{monster.name}</h1></div>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
