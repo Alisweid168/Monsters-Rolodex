79ebb78b4b95745bc1d08a988da93152120c69a2
Alisweid168 <ali.sweidan003@gmail.com>
2024-03-29 01:27:08 +0200

    update-application

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -25,8 +27,12 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+      <input className="search-box" type="search" placeholder="search monsters" onChange={(event)=>{
+        console.log(event.target.value);
+      }}/>
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
